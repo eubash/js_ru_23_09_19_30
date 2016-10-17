3ac7f54230635cabc7be071a58ce2d5bfbea9130
Roman <kaboom.ua@gmail.com>
2016-10-17 21:24:28 +0300

    Update generateId.js

--- src/middlewares/generateId.js
+++ src/middlewares/generateId.js
@@ -3,7 +3,9 @@ import { ADD_COMMENT } from '../constants'
 export default store => next => action => {
     const { type, payload } = action
 
-    switch (type) {
+    //Плохо, мидлвары должны быть максимально универсальными, стоит делать так, что б их легко можно было переиспользовать
+    //плохая логика для генерации id, нет никакой гарантии уникальности; Лучше просто рандом
+   switch (type) {
         case ADD_COMMENT:
             const comments = store.getState().comments
             const lastId = Math.max(...Object.keys(comments))  // get max id
